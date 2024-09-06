5f6ae47228dbc74e9cf5a89afd3d08992e0b3cf7
koutaiba141 <koutaibadiab23@gmail.com>
2024-09-06 21:52:27 +0300

    adding persons migration

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-db', function () {
+    try {
+        $dbName = DB::connection()->getDatabaseName();
+        return 'Connected to database: ' . $dbName;
+    } catch (\Exception $e) {
+        return 'Could not connect to the database. Error: ' . $e->getMessage();
+    }
+});
