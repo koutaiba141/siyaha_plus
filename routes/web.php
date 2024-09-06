a68ca1afaad2996220300d1f1816035c7506867a
koutaiba141 <koutaibadiab23@gmail.com>
2024-09-06 21:57:01 +0300

    removing unneeded function

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test-db', function () {
-    try {
-        $dbName = DB::connection()->getDatabaseName();
-        return 'Connected to database: ' . $dbName;
-    } catch (\Exception $e) {
-        return 'Could not connect to the database. Error: ' . $e->getMessage();
-    }
-});
